5c3bf6d375e72ab2e8ba2214e9b96015374398de
Enzo Rafael <enzoricarteaa@gmail.com>
2024-01-18 12:15:10 -0300

    implementação do dash

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,48 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    //Movement
     public float speed = 6.0f;
     Rigidbody rb;
-    void Start() {
+
+    //Dash
+    private bool canDash = true;
+    private bool isDashing;
+    private float dashingPower = 100f;
+    private float dashingTime = 1f;
+    private float dashingCooldown = 1f;
+    TrailRenderer tr;
+
+    void Start()
+    {
     rb = GetComponent<Rigidbody>();
+    tr = GetComponent<TrailRenderer>();
     }
-    void Update() {
-    float h = Input.GetAxis("Horizontal") * speed * Time.deltaTime ; ;
-    float v = Input.GetAxis("Vertical") * speed * Time.deltaTime ; ;
+
+    void Update()
+    {
+    float h = Input.GetAxis("Horizontal") * speed * Time.deltaTime ;
+    float v = Input.GetAxis("Vertical") * speed * Time.deltaTime ;
     Vector3 dir = new Vector3(h, rb.velocity.y, v);
     rb.velocity = dir * speed;
+    if(Input.GetKeyDown(KeyCode.Space) && canDash)
+    {
+        StartCoroutine(Dash());
+    }
+    }
+
+    private IEnumerator Dash()
+    {
+        canDash = false;
+        isDashing = true;
+        rb.useGravity = false;
+        rb.velocity = new Vector3(transform.localScale.x * dashingPower,0f,0f);
+        tr.emitting = true;
+        yield return new WaitForSeconds(dashingTime);
+        tr.emitting = false;
+        rb.useGravity = true;
+        isDashing = false;
+        yield return new WaitForSeconds(dashingCooldown);
+        canDash = true;
     }
 }
