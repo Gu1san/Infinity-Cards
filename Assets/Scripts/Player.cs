06a7d913bd0fcf7d40328ce87fa84e68bdb21cf3
Enzo Rafael <enzoricarteaa@gmail.com>
2024-01-22 13:27:03 -0300

    Implementação da mira

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -18,6 +18,11 @@ public class Player : MonoBehaviour
     private float dashingCooldown = 1f;
     TrailRenderer tr;
 
+    //Shoot
+    public Transform mira;
+    public Transform refArma;
+
+
     void Start()
     {
     rb = GetComponent<Rigidbody2D>();
@@ -26,17 +31,26 @@ public class Player : MonoBehaviour
 
     void Update()
     {
-    /*float h = Input.GetAxis("Horizontal") * speed * Time.deltaTime ;
-    float v = Input.GetAxis("Vertical") * speed * Time.deltaTime ;
-    Vector3 dir = new Vector3(h, rb.velocity.y, v);
-    rb.velocity = dir * speed;*/
+    //detectar o mause e mira
+    mira.position = Camera.main.ScreenToWorldPoint(new Vector3( 
+        Input.mousePosition.x,
+        Input.mousePosition.y,
+        -Camera.main.transform.position.z
+    ));
+    
+
     movmentDirection = new Vector2(Input.GetAxisRaw("Horizontal"),Input.GetAxisRaw("Vertical"));
 
     if(Input.GetKeyDown(KeyCode.Space) && canDash)
     {
         StartCoroutine(Dash());
     }
+
+    if(Input.GetKeyDown(KeyCode.Mouse0)){
+       Shoot();
     }
+    }
+
     void FixedUpdate(){
         rb.MovePosition(rb.position + movmentDirection * speed * Time.fixedDeltaTime);
     }
@@ -55,5 +69,15 @@ public class Player : MonoBehaviour
         isDashing = false;
         yield return new WaitForSeconds(dashingCooldown);
         canDash = true;
+    }
+    
+    void Shoot(){
+      RaycastHit2D hit = Physics2D.Raycast(refArma.position, (mira.position - refArma.position).normalized,1000f, ~(1<<6));
+      if(hit.collider != null){
+        if (hit.collider.gameObject.CompareTag("Enemy")){
+        Debug.Log("hit");
+        //Destroy(hit.collider.gameObject);
+       }
+      }
     }
 }
